564c154673973e6fd746d21dbe6d0742b0f42131
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-06-12 09:18:57 +0530

    12june

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+export default function App() {
+  return (
+    <>
+  <Text>Hellp</Text>
+  <Text>Hellp</Text>
+  <Text>Hellp</Text>
+  <Text>Hellp</Text>
+</>
+  
+  )
+}
