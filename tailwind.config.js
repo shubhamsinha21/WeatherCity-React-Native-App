44f18fcba8c27813c32548ab55d90cbf441a52c3
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-06-12 10:14:41 +0530

    native-wind added (tailwind css)

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx}", "./screens/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
